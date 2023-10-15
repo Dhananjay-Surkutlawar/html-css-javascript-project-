435188a0171ec5589f891d83f8e50f8a8e1f1bf2
dhananjay <dhananjay382000@gmail.com>
2023-10-15 11:24:30 +0530

    bmi calculator

---
+++ BMI Calculator/formula.js
@@ -0,0 +1,30 @@
+window.onload=()=>{
+    // console.log("helo")
+    const button =document.querySelector('#btn')
+    button.addEventListener('click',calculate)
+
+ }
+
+ function calculate()
+ {
+    const weight = document.getElementById('Weight').value;
+    const height = document.getElementById('Height').value;
+    const result= document.querySelector('.result')
+
+    if(!height || height<=0 || !weight || weight<= 0)
+   {
+       result.innerText="Enter valid height or code"
+   }
+   else
+   {
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
+
+    result.innerText=`Your BMI is ${bmi.toFixed(2)}`
+
+   }
+
+
+
+
+ }
